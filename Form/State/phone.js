747d3c23708c011f7ee8dc6c914f80bae887c088
tanveerisonline <tanveerisonline@gmail.com>
2021-07-07 16:08:22 +0530

    country code field added

--- Form/State/phone.js
+++ Form/State/phone.js
@@ -1,10 +1,5 @@
-const info = document.querySelector(".alert-info");
-
-function process(event) {
- event.preventDefault();
-
- const phoneNumber = phoneInput.getNumber();
-
- info.style.display = "";
- info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
-}
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
